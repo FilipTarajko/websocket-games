62b8f227d11a925a3361be1c9bd0502fc7418edf
Filip Tarajko <filiptarajko@gmail.com>
2024-03-21 23:09:27 +0100

    Improve rooms list UI

--- frontend/src/components/RoomsList.vue
+++ frontend/src/components/RoomsList.vue
@@ -4,6 +4,15 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useRoomsStore } from '@/stores/roomsStore';
 import { useSocketStore } from '@/stores/socketStore';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 const roomsStore = useRoomsStore()
 const socketStore = useSocketStore()
@@ -21,17 +30,42 @@ function joinRoom(id: number) {
 
 <template>
   <div>
-    <div v-for="room in roomsStore.rooms">
-      {{ room.id }} | {{ room.name }} | {{ room.usersLenght }} | {{ room.gameName }} | {{
-        room.public ? "public" : "private"
-      }}
-      <Button @click="() => { joinRoom(room.id) }" size="sm"
-        :disabled="!room.public || room.id == roomsStore.currentRoom.id">join</Button>
-    </div>
-    <Button @click="() => { joinRoom(1) }" :disabled="roomsStore.currentRoom.id == 1">back to lobby</Button>
-    <div class="flex mt-4">
-      <Input v-model="newRoomName" class="mr-2" type="text" id="newRoomName" />
-      <Button @click="createRoom">create room</Button>
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Id</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Users</TableHead>
+          <TableHead>Game</TableHead>
+          <TableHead>Public</TableHead>
+          <TableHead>Join</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow v-for="room in roomsStore.rooms" :key="room.id">
+          <TableCell>{{ room.id }}</TableCell>
+          <TableCell>{{ room.name }}</TableCell>
+          <TableCell>{{ room.usersLenght }}</TableCell>
+          <TableCell>{{ room.gameName }}</TableCell>
+          <TableCell>{{ room.public ? "public" : "private" }}</TableCell>
+          <TableCell>
+            <Button @click="() => { joinRoom(room.id) }" size="sm"
+              :disabled="!room.public || room.id == roomsStore.currentRoom.id">
+              <span v-if="room.id == roomsStore.currentRoom.id">you are here</span>
+              <span v-else-if="!room.public">room is private</span>
+              <span v-else>join</span>
+            </Button>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell colspan="1"><Input v-model="newRoomName" class="mr-2" type="text" id="newRoomName" /></TableCell>
+          <TableCell><Button @click="createRoom">create room</Button></TableCell>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   </div>
 </template>
