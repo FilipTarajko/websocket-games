21fa7226e2acccc3fbe80f4d5643114f2a33301d
ft <filiptarajko@gmail.com>
2024-05-01 04:35:18 +0200

    Allow chat to stay scrolled to bottom on new message

--- frontend/src/components/ChatComponent.vue
+++ frontend/src/components/ChatComponent.vue
@@ -15,16 +15,21 @@ function sayInRoom() {
 </script>
 
 <template>
-  <div id="chat" class="border-solid border-2 border-gray-500 overflow-auto mb-2">
-    <template v-for="message in socketStore.chatMessages">
-      <div v-if="message.author" class="pl-1">
-        <span v-text="message.author.username"></span>:
-        <span v-text="message.message"></span>
-      </div>
-      <div v-else style="background-color: hsla(120, 100%, 50%, 0.2)" class="my-1 pl-1">
-        <span v-text="message"></span>
-      </div>
-    </template>
+  <div
+    id="chat"
+    class="border-solid border-2 border-gray-500 overflow-auto mb-2 flex flex-col-reverse"
+  >
+    <div>
+      <template v-for="message in socketStore.chatMessages">
+        <div v-if="message.author" class="pl-1">
+          <span v-text="message.author.username"></span>:
+          <span v-text="message.message"></span>
+        </div>
+        <div v-else style="background-color: hsla(120, 100%, 50%, 0.2)" class="my-1 pl-1">
+          <span v-text="message"></span>
+        </div>
+      </template>
+    </div>
   </div>
   <form class="flex" @submit.prevent="sayInRoom">
     <Input v-model="messageToSay" type="text" id="messageText" />
